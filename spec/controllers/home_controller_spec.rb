5651bfee1599e07795d2edb5501fbbcac550b4c2
martensonbj <martensonbj@gmail.com>
2016-02-28 18:31:30 -0700

    Create landing page with facebook login

---
+++ spec/controllers/home_controller_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+RSpec.describe HomeController, type: :controller do
+  describe "GET #index" do
+
+    it "responds with success" do
+      get :index
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
+
+    it "renders the index template" do
+      get :index
+      expect(response).to render_template("index")
+    end
+
+  end
+end
---
+++ spec/features/landing_page_spec.rb
@@ -0,0 +1,14 @@
+require "rails_helper"
+
+RSpec.feature "landing page", :type => :feature do
+  scenario "Uesr visits the landing page" do
+    visit "/"
+
+    within '.home-content' do
+      expect(page).to have_text("What Are The Odds?")
+    end
+    within '.login' do
+      expect(page).to have_text("Login with Facebook")
+    end
+  end
+end
