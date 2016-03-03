170e8bb94c82c5fa8a0614cc478f135f6a6f94cd
martensonbj <martensonbj@gmail.com>
2016-03-02 17:17:07 -0700

    Add enum to set challenge status

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,3 +1,13 @@
 class Challenge < ActiveRecord::Base
   belongs_to :user
+  helper_method :user_has_pending_challenges
+  enum status: %w(pending, completed, declined)
+
+  private user_has_pending_challenges
+    my_challenges = Challenge.where(assigned_user: current_user.id)
+    my_challenges.any? do |challenge|
+      challenge.status == "pending"
+    end
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
-class User < ActiveRecord::Base
+  class User < ActiveRecord::Base
   has_many :challenges
   has_many :friendships
   has_many :friends, :through => :friendships
