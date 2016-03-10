7f3f555be9f124f9d842f26a9f0919c3ae9fc7dd
martensonbj <martensonbj@gmail.com>
2016-03-10 00:34:27 -0700

    All tests passing

--- spec/models/friend_builder_spec.rb
+++ spec/models/friend_builder_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe FriendBuilder, type: :model do
-  xit "creates an array of friends" do
+  it "creates an array of friends" do
     VCR.use_cassette 'create_friends' do
-      user = OmniAuth.config.mock_auth[:facebook]
+      user = generate_user
       fb = FriendBuilder.new
-    friends = fb.build_friends(user)
+      friends = fb.build_friends(user)
 
       expect(friends.class).to eq (Array)
     end
