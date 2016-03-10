1336ea02634b30abbbc16ef509b510af60859e37
martensonbj <martensonbj@gmail.com>
2016-03-09 20:35:46 -0700

    Remove lines in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,6 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
-  Paperclip.options["/usr/local/bin/convert"] = "/usr/local/bin/"
 
   config.paperclip_defaults = {
     :storage => :s3,
