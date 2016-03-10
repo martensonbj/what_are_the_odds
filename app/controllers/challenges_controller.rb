b383baed2cb46d4905d51a9e753f2eb67b16efc7
martensonbj <martensonbj@gmail.com>
2016-03-10 01:17:35 -0700

    Images showing up on show page

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -5,17 +5,15 @@ class ChallengesController < ApplicationController
   before_action :get_images, only: [:show]
 
   def index
-      @sent_challenges = Challenge.all.where(user_id: current_user.id)
-      @pending_challenges = Challenge.all.where(assigned_user: current_user.id.to_s)
+    @sent_challenges = Challenge.get_sent_challenges(current_user)
+    @pending_challenges = Challenge.get_pending_challenges(current_user)
   end
 
   def new
     @challenge = Challenge.new
-    fb = FriendBuilder.new
-    @friends = fb.build_friends(current_user)
-    fs = FacebookService.new(current_user)
-    fs.email
-  end
+    @friends = @challenge.get_friends(current_user)
+    @recorded_image = session[:current_image].split('/').last
+ end
 
   def edit
   end
@@ -38,12 +36,6 @@ class ChallengesController < ApplicationController
   end
 
   def show
-    bucket = Bucket.new
-    images = bucket.get_aws_photos
-    challenge_key = @challenge.challenge_video
-    response_key = @challenge.response_video
-    @challenge_video = images[challenge_key]
-    @response_video = images[response_key]
   end
 
   def destroy
@@ -67,12 +59,23 @@ class ChallengesController < ApplicationController
   end
 
   def get_images
-    bucket = Bucket.new
-    images = bucket.get_aws_photos
-    challenge_key = @challenge.challenge_video
-    response_key = @challenge.response_video
-    @challenge_video = images[challenge_key]
-    @response_video = images[response_key]
+    images = Bucket.new.get_aws_photos
+    @challenge_video = get_challenge_video(images)
+    @response_video = get_response_video(images)
+  end
+
+  def get_challenge_video(images)
+    if @challenge.challenge_video
+      challenge_key = @challenge.challenge_video
+      @challenge_video = images[challenge_key]
+    end
+  end
+
+  def get_response_video(images)
+    if @challenge.response_video
+      response_key = @challenge.response_video
+      @response_video = images[response_key]
+    end
   end
 
 end
--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,13 +1,13 @@
 class Challenge < ActiveRecord::Base
   belongs_to :user
   has_many :posts, dependent: :destroy
+  has_many :headshot_photos, :as => :capturable
   enum status: %w(pending completed accepted declined activated dead)
 
   validates :title, presence: true
   validates :message, presence: true
   validates :assigned_user, presence: true
 
-  has_many :headshot_photos, :as => :capturable
 
   has_attached_file :image_upload
   do_not_validate_attachment_file_type :image_upload
@@ -22,6 +22,19 @@ class Challenge < ActiveRecord::Base
 
   def challengee_accepted?
     self.challengee_guess && !self.challenger_guess
+  end
+
+  def self.get_sent_challenges(current_user)
+    self.all.where(user_id: current_user.id)
+  end
+
+  def self.get_pending_challenges(current_user)
+    self.all.where(assigned_user: current_user.id.to_s)
+  end
+
+  def get_friends(current_user)
+    fb = FriendBuilder.new
+    fb.build_friends(current_user)
   end
 
 end
